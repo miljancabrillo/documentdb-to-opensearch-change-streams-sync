c592e26166fe1b84138eb6b366722331d264f2de
miljancabrillo <miljancabrilo@yahoo.com>
2023-07-09 21:23:51 +0200

    Remove env from stack props

--- bin/documentdb-to-opensearch-change-streams-sync.ts
+++ bin/documentdb-to-opensearch-change-streams-sync.ts
@@ -6,31 +6,22 @@ import { DocumentDbStack } from '../lib/documentdb.stack';
 import { OpenSearchStack } from '../lib/opensearch.stack';
 import { LambdaStack } from '../lib/lambda.stack';
 
-const env = {
-    region: 'eu-west-1'
-};
-
 const app = new App();
 Tags.of(app).add('Application', 'change-streams-demo');
 
-const vpcStack = new VpcStack(app, 'change-streams-demo-vpc-stack', {
-    env
-});
+const vpcStack = new VpcStack(app, 'change-streams-demo-vpc-stack');
 
 const documentDbStack = new DocumentDbStack(app, 'change-streams-demo-documentdb-stack', {
-    env,
     vpc: vpcStack.vpc,
     documentDbSecurityGroup: vpcStack.documentDbSecurityGroup
 });
 
 const openSearchStack = new OpenSearchStack(app, 'change-streams-demo-opensearch-stack', {
-    env,
     vpc: vpcStack.vpc,
     openSearchSecurityGroup: vpcStack.openSearchSecurityGroup
 });
 
 new LambdaStack(app, 'change-streams-demo-lambda-stack', {
-    env,
     vpc: vpcStack.vpc,
     lambdSecurityGroup: vpcStack.lambdaSecurityGroup,
     documentDbSecretArn: documentDbStack.getSecretArn(),
